0c3c94ab9bc8f78b9f141336d56f0d54a3015f80
Neva Alfaizu Kurniawan <74447299+nevaalfaizu@users.noreply.github.com>
2025-06-12 19:40:13 +0700

    tambah cors

--- app/startapi.go
+++ app/startapi.go
@@ -3,6 +3,9 @@ package app
 import (
 	"strconv"
 	"tes1/varglobal"
+	"time"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +14,15 @@ func StartApi() {
 	// pakai gin untuk api backend
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*", "http://localhost:8080", "http://localhost"}, // Ganti dengan origin spesifik jika perlu
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// panggil router
 	Router(r)
